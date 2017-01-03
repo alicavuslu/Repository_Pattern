cc407baec3ac07cdbe023c89c39bb2334e9fec10
alicavuslu <alicavuslu@gmail.com>
2017-01-03 14:43:34 +0300

    Repository Pattern example
    Repository pattern example

---
+++ EF.RP/EF.Business/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Data.Objects;
+using EF.Data;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data.Entity.Core.Objects;
+//using System.Data.Linq;
+
+namespace EF.Bussines
+{
+    public class Repository<T> where T : class
+    {
+        ObjectContext _context;
+        IObjectSet<T> _objectSet;
+
+        //Context Oluşturuyoruz
+
+        //DenemeEntities db = new DenemeEntities();
+
+        public Repository()
+        {
+            // ObjectContext nesnesi oluşturyoruz.
+
+            //_context = (ObjectContext)db;
+            _objectSet = _context.CreateObjectSet<T>();
+        }
+
+        // Entity Göre listeleme yapan generic metodumuz
+        public List<T> Listele()
+        {
+            List<T> liste = _objectSet.ToList();
+            return liste;
+        }
+
+        // Lambda syntax kullnarak sorgularımızı yaptığımız metot
+        public List<T> SorguyaGoreListele(Expression<Func<T, bool>> where)
+        {
+            return _objectSet.Where(where).ToList();
+        }
+
+    }
+}
---
+++ EF.RP/EF.Client/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using EF.Business;
+//using EF.Data;
+
+namespace EF.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Tüm Personelimiz Listeler
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //Repository<Personel> ds = new Repository<Personel>();
+
+            //dataGrid1.ItemsSource = ds.Listele();
+        }
+
+        // Tüm Şirketleri Listeler
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //Repository<Sirket> ds = new Repository<Sirket>();
+            //dataGrid1.ItemsSource = ds.Listele();
+        }
+
+        // Gelen  Sorgulara Göre Listeleme Yapar
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            //Repository<Personel> ps = new Repository<Personel>();
+
+            int sirketid = Convert.ToInt32(textBox1.Text);
+
+            //dataGrid1.ItemsSource = ps.SorguyaGoreListele(p => p.Sirketid == sirketid);
+        }
+    }
+}
